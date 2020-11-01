4815461dc4df4014cafcff7aa84a743aee1983a3
Marcelo Silveira Vilar <tchelovilar@gmail.com>
2020-11-01 17:59:08 +0100

    Updated metrics to add a labels label and small fixies

--- src/runner-exporter.py
+++ src/runner-exporter.py
@@ -21,7 +21,9 @@ class runnerExports:
     def __init__(self):
         # Define metrics to expose
         self.metric_runner_org_status = Gauge(
-            "github_runner_org_status", "Runner status", ["name", "id", "os", "status"]
+            "github_runner_org_status",
+            "Runner status",
+            ["name", "id", "os", "labels", "status"],
         )
         self.metric_runner_org_label_status = Gauge(
             "github_runner_org_label_status",
@@ -30,37 +32,52 @@ class runnerExports:
         )
 
         self.metric_runner_org_busy = Gauge(
-            "github_runner_org_busy", "Runner busy status", ["name", "id", "os", "busy"]
+            "github_runner_org_busy",
+            "Runner busy status",
+            ["name", "id", "os", "labels", "busy"],
         )
 
     def export_metrics(self, runner_list: list):
         current_runners = []
 
         for runner in runner_list:
-            self.export_runner_status(runner)
-            self.export_runner_busy(runner)
+            agg_labels = self.aggregate_labels(runner["labels"])
+            # Export metrics
+            self.export_runner_status(runner, agg_labels)
+            self.export_runner_busy(runner, agg_labels)
+            # Updated active runners list
             current_runners.append(str(runner["id"]))
 
         self.ghostbuster(current_runners)
 
     def ghostbuster(self, current_runners):
+        """
+            Case some runner is deleted this function will remove from the metrics
+        """
+        # Remove ghosts form metric_runner_org_status metric
         runners_to_remove = []
         for (
             runner_name,
             runner_id,
             runner_os,
+            labels,
             runner_status,
         ) in self.metric_runner_org_status._metrics:
             if runner_id not in current_runners:
                 runners_to_remove.append(
-                    (runner_name, runner_id, runner_os, runner_status)
+                    (runner_name, runner_id, runner_os, labels, runner_status)
                 )
-        # Remove ghosts
-        for runner_name, runner_id, runner_os, runner_status in runners_to_remove:
+        for (
+            runner_name,
+            runner_id,
+            runner_os,
+            labels,
+            runner_status,
+        ) in runners_to_remove:
             self.metric_runner_org_status.remove(
-                runner_name, runner_id, runner_os, runner_status
+                runner_name, runner_id, runner_os, labels, runner_status
             )
-
+        # Remove ghosts form metric_runner_org_label_status metric
         runners_to_remove = []
         for (
             runner_name,
@@ -73,7 +90,6 @@ class runnerExports:
                 runners_to_remove.append(
                     (runner_name, runner_id, runner_os, runner_label, runner_status)
                 )
-        # Remove ghosts
         for (
             runner_name,
             runner_id,
@@ -84,25 +100,36 @@ class runnerExports:
             self.metric_runner_org_label_status.remove(
                 runner_name, runner_id, runner_os, runner_label, runner_status
             )
-
+        # Remove ghosts form metric_runner_org_busy metric
         runners_to_remove = []
         for (
             runner_name,
             runner_id,
             runner_os,
+            labels,
             runner_busy,
         ) in self.metric_runner_org_busy._metrics:
             if runner_id not in current_runners:
                 runners_to_remove.append(
-                    (runner_name, runner_id, runner_os, runner_busy)
+                    (runner_name, runner_id, runner_os, labels, runner_busy)
                 )
-        # Remove ghosts
-        for runner_name, runner_id, runner_os, runner_busy in runners_to_remove:
+        for runner_name, runner_id, runner_os, labels, runner_busy in runners_to_remove:
             self.metric_runner_org_busy.remove(
-                runner_name, runner_id, runner_os, runner_busy
+                runner_name, runner_id, runner_os, labels, runner_busy
             )
 
-    def export_runner_status(self, runner: dict):
+    def aggregate_labels(self, labels: dict):
+        """
+            Aggregate the runners labels in string
+        """
+        agg_labels = []
+        for label in labels:
+            if label["type"] == "custom":
+                agg_labels.append(label["name"])
+
+        return ",".join(agg_labels)
+
+    def export_runner_status(self, runner: dict, agg_labels: str):
         online = 1
         offline = 0
         if runner.get("status") != "online":
@@ -110,10 +137,14 @@ class runnerExports:
             offline = 1
 
         self.metric_runner_org_status.labels(
-            runner.get("name"), runner.get("id"), runner.get("os"), "online"
+            runner.get("name"), runner.get("id"), runner.get("os"), agg_labels, "online"
         ).set(online)
         self.metric_runner_org_status.labels(
-            runner.get("name"), runner.get("id"), runner.get("os"), "offline"
+            runner.get("name"),
+            runner.get("id"),
+            runner.get("os"),
+            agg_labels,
+            "offline",
         ).set(offline)
 
         for label in runner["labels"]:
@@ -133,10 +164,21 @@ class runnerExports:
                 "offline",
             ).set(offline)
 
-    def export_runner_busy(self, runner: dict):
+    def export_runner_busy(self, runner: dict, agg_labels: str):
+        idle = 1
+        busy = 0
+
+        if runner.get("busy") == True:
+            idle = 0
+            busy = 1
+
         self.metric_runner_org_busy.labels(
-            runner.get("name"), runner.get("id"), runner.get("os"), runner.get("busy")
-        ).set(1)
+            runner.get("name"), runner.get("id"), runner.get("os"), agg_labels, "true"
+        ).set(busy)
+
+        self.metric_runner_org_busy.labels(
+            runner.get("name"), runner.get("id"), runner.get("os"), agg_labels, "false"
+        ).set(idle)
 
 
 def main():
